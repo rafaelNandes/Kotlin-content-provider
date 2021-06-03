c63082659470a9319f51e909599e8fb931d157dd
rafaelNandes <rafael.fernandessilva@hotmail.com>
2021-06-03 20:11:35 -0300

    configurating database methods in content provider

--- README.md
+++ README.md
@@ -6,7 +6,17 @@
 
 **Features:**
 
+- Android API 16
+
 - SQLite
+
+
+
+**Orientations:**
+
+In Content Provider Class I used the UnsupportedSchemeException which is available from Android API 18 onwards. So I used the notation @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2) in the methods.
+
+
 
 
 
--- app/src/main/java/com/example/contentprovider/database/NotesProvider.kt
+++ app/src/main/java/com/example/contentprovider/database/NotesProvider.kt
@@ -2,11 +2,21 @@ package com.example.contentprovider.database
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.Context
 import android.content.UriMatcher
 import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.media.UnsupportedSchemeException
 import android.net.Uri
+import android.os.Build
+import android.provider.BaseColumns._ID
+import androidx.annotation.RequiresApi
+import com.example.contentprovider.database.NotesDatabaseHelper.Companion.TABLE_NOTES
 
 class NotesProvider : ContentProvider() {
+
+    private lateinit var mUriMatcher: UriMatcher
+    private lateinit var dbHelper: NotesDatabaseHelper
 
     companion object {
         const val AUTHORITY = "com.example.contentprovider.provider"
@@ -16,40 +26,84 @@ class NotesProvider : ContentProvider() {
         val URI_NOTES = Uri.withAppendedPath(BASE_URI, "notes")
     }
 
-    private lateinit var mUriMatcher: UriMatcher
     override fun onCreate(): Boolean {
         mUriMatcher = UriMatcher(UriMatcher.NO_MATCH)
         mUriMatcher.addURI(AUTHORITY, "notes", NOTES)
         mUriMatcher.addURI(AUTHORITY, "notes/#", NOTES_BY_ID)
+        if(context != null){
+            dbHelper = NotesDatabaseHelper(context as Context)
+        }
         return true
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2) //UnsupportedSchemeException is available to API 18 and the current is API 16
     override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
-        TODO("Implement this to handle requests to delete one or more rows")
+        if(mUriMatcher.match(uri) == NOTES_BY_ID){
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffect = db.delete(TABLE_NOTES, "$_ID =?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffect
+        } else {
+            throw UnsupportedSchemeException("Invalid uri to delete!")
+        }
     }
 
-    override fun getType(uri: Uri): String? {
-        TODO(
-            "Implement this to handle requests for the MIME type of the data" +
-                    "at the given URI"
-        )
-    }
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2) //UnsupportedSchemeException is available to API 18 and the current is API 16
+    override fun getType(uri: Uri): String? = throw UnsupportedSchemeException("Uri not implemented!")
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2) //UnsupportedSchemeException is available to API 18 and the current is API 16
     override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        TODO("Implement this to handle requests to insert a new row.")
+        if(mUriMatcher.match(uri) == NOTES){
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val id = db.insert(TABLE_NOTES, null, values)
+            val insertUri = Uri.withAppendedPath(BASE_URI, id.toString())
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return insertUri
+        } else {
+            throw UnsupportedSchemeException("Invalid uri to insert!")
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2) //UnsupportedSchemeException is available to API 18 and the current is API 16
     override fun query(
         uri: Uri, projection: Array<String>?, selection: String?,
         selectionArgs: Array<String>?, sortOrder: String?
     ): Cursor? {
-        TODO("Implement this to handle query requests from clients.")
+        return when{
+            mUriMatcher.match(uri) == NOTES -> {
+                val db:SQLiteDatabase = dbHelper.writableDatabase
+                val cursor = db.query(TABLE_NOTES, projection, selection, selectionArgs, null, null, sortOrder)
+                cursor.setNotificationUri(context?.contentResolver, uri)
+                cursor
+            }
+            mUriMatcher.match(uri) == NOTES_BY_ID -> {
+                val db: SQLiteDatabase = dbHelper.writableDatabase
+                val cursor = db.query(TABLE_NOTES, projection, "$_ID =?", arrayOf(uri.lastPathSegment), null, null, setOrder)
+                cursor.setNotificationUri(context?.contentResolver, uri)
+                cursor
+            }
+            else -> {
+                throw UnsupportedSchemeException("Uri not implemented")
+            }
+
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2) //UnsupportedSchemeException is available to API 18 and the current is API 16
     override fun update(
         uri: Uri, values: ContentValues?, selection: String?,
         selectionArgs: Array<String>?
     ): Int {
-        TODO("Implement this to handle requests to update one or more rows.")
+        if(mUriMatcher.match(uri) == NOTES_BY_ID){
+            val db: SQLiteDatabase = dbHelper.writableDatabase
+            val linesAffect = db.update(TABLE_NOTES, values, "$_ID =?", arrayOf(uri.lastPathSegment))
+            db.close()
+            context?.contentResolver?.notifyChange(uri, null)
+            return linesAffect
+        } else {
+            throw UnsupportedSchemeException("Uri not implemented!")
+        }
     }
 }
