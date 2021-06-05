68516ed8b0ddbd261095cddf4d7bbc70d35747bd
rafaelNandes <rafael.fernandessilva@hotmail.com>
2021-06-05 20:33:37 -0300

    creating clientadapter of the recycleview

--- Kotlin-content-provider-client/app/src/main/java/com/example/contentproviderclient/MainActivity.kt
+++ Kotlin-content-provider-client/app/src/main/java/com/example/contentproviderclient/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.contentproviderclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var notesRecycler:RecyclerView
+    lateinit var noteRefreshButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        notesRecycler = findViewById(R.id.client_list)
+        noteRefreshButton = findViewById(R.id.client_button_refresh)
+        noteRefreshButton.setOnClickListener {  }
     }
 }
