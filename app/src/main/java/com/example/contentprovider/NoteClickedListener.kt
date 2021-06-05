8d61fa2be1eb1a2eed08f3cce1050040e7a434d9
Rafael Fernandes <fernandes2.rfs@gmail.com>
2021-06-05 19:44:22 -0300

    Delete app directory
    removing duplicated files

--- app/src/main/java/com/example/contentprovider/NoteClickedListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.contentprovider
-
-import android.database.Cursor
-
-interface NoteClickedListener {
-    fun noteClickedItem(cursor: Cursor)
-    fun noteRemoveItem(cursor: Cursor?)
-}
